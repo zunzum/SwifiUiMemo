e179d21fbe94a425ce0f58fe0cdff2211174136e
zunzum <zunzum@naver.com>
2022-10-28 14:40:36 +0900

    기초작업

---
+++ SwifiUiMemo/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  SwifiUiMemo
+//
+//  Created by 동준 on 2022/10/28.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
--- SwifiUiMemo/SwifiUiMemoApp.swift
+++ SwifiUiMemo/SwifiUiMemoApp.swift
@@ -13,7 +13,7 @@ struct SwifiUiMemoApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
