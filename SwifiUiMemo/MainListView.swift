c31c7ffe14436c0949cd51c325a406d0d7efd7ad
zunzum <zunzum@naver.com>
2022-10-28 15:22:29 +0900

    Step3 보여주기 수정

--- SwifiUiMemo/MainListView.swift
+++ SwifiUiMemo/MainListView.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct MainListView: View {
+    @EnvironmentObject var store : MemoStore
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(store.list) { memo in
+                VStack(alignment: .leading) {
+                    Text(memo.content)
+                        .font(.body)
+                        .lineLimit(1)
+                    Text(memo.insertDate, style: .date)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("내 메모")
+        }
+        
     }
 }
 
 struct MainListView_Previews: PreviewProvider {
     static var previews: some View {
         MainListView()
+            .environmentObject(MemoStore())
     }
 }
